927251b1ca0f44729c1fb4f35d096452e2fa8462
muhammadali <101570725+muhammadali-nb@users.noreply.github.com>
2025-12-10 22:17:23 +0500

    feat: add scroll behavior to router for improved navigation experience

--- src/router/router.ts
+++ src/router/router.ts
@@ -6,6 +6,13 @@ import routes from './routes.ts';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 router.beforeEach(layoutMiddleware);
